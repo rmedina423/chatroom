72226cecd04a48e46aca48372a4cde10152aeccc
Ryan M. Medina <rmedina423@gmail.com>
2016-05-01 15:07:46 -0700

    add helper function to process raw user input

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -59,3 +59,23 @@ function divEscapeContentElement(message) {
 function divSystemContentElement(message) {
 	return $('<div></div>').html('<li>' + message + '</li>');
 }
+
+function processUserInput(chatApp, socket) {
+	var message = $('#send-message').val();
+	var systemMessage;
+
+	if (message.charAt(0) == '/') {
+		systemMessage = chatApp.processCommand(message);
+
+		if (systemMessage) {
+			$('#messages').append(divSystemContentElement(systemMessage));
+		}
+	} else {
+		chatApp.sendMessages($('#room').text(), message);
+
+		$('#messages').append(divEscapeContentElement(systemMessage));
+		$('#messages').scrollTop($('#messages').prop('scrollHeight'));
+	}
+
+	$('#send-message').val();
+}
