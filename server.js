a2b7b3ed1828996a74a941ebac9cec3d024b635a
Ryan M. Medina <rmedina423@gmail.com>
2016-05-01 13:59:01 -0700

    add comments

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
+// Built-in http module provides HTTP server and client functionality
 var http = require('http');
+
+// Built-in fs module provides filesystem related functionality
 var fs = require('fs');
+
+// Build-in path module provides filesystem path-related functionality
 var path = require('path');
+
+// Add-on mime module provides ability to derive a MIME type based on a filesystem exentsion
 var mime = require('mime');
+
+// cache object is where the contents of cached files are stored
 var cache = {};
 
+// Create HTTP server, using anonymous function to define per-request behavior
 var server = http.createServer(function (request, response) {
+
 	var filePath = false;
+
 	if (request.url == '/') {
+
+		// Determine HTML file to be served by default
 		filePath = 'public/index.html';
+		
 	} else {
+
+		// Translate URL path to relative file path
 		filePath = 'public' + request.url;
 	}
+
 	var absPath = './' + filePath;
+
+	// Serve static file
 	serveStatic(response, cache, absPath);
 })
 
@@ -33,25 +53,39 @@ function sendFile(response, filePath, fileContents) {
 		200,
 		{"content-type": mime.lookup(path.basename(filePath))}
 	);
-	console.log(fileContents);
 	response.end(fileContents);
 }
 
 function serveStatic(response, cache, absPath) {
+
+	// Check if file is cached in memory
 	if (cache[absPath]) {
+
+		// Serve file from memory
 		sendFile(response, absPath, cache[absPath]);
+
 	} else {
+
+		// Check if file exists
 		fs.exists(absPath, function (exists) {
 			if (exists) {
+
+				// Read file from disk
 				fs.readFile(absPath, function (err, data) {
+
 					if (err) {
 						send404(err);
 					} else {
 						cache[absPath] = data;
+
+						// Serve file read from disk
 						sendFile(response, absPath, data);
 					}
 				});
+
 			} else {
+
+				// Send HTTP 404 response
 				send404(response);
 			}
 		})
