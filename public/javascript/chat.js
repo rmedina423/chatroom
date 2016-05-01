cacf874aa5f86c75141b512f87e694173faccaf0
Ryan M. Medina <rmedina423@gmail.com>
2016-05-01 14:51:13 -0700

    add functionality to relay messages and name/room changes to the server

---
+++ public/javascript/chat.js
@@ -0,0 +1,53 @@
+var Chat = function (socket) {
+	this.socket = socket;
+};
+
+Chat.prototype.sendMessages = function (room, text) {
+	var message = {
+		room: room,
+		text: text
+	};
+
+	this.socket.emit('message', message);
+};
+
+Chat.prototype.changeRoom = function (room) {
+	this.socket.emit('join', {
+		newRoom: room
+	});
+};
+
+Chat.prototype.processCommand = function (command) {
+	var words = command.split(' ');
+
+	// Parse command from first word
+	var command = words[0].substring(1, words[0].length).toLowerCase();
+	var message = false;
+
+	switch(command) {
+
+		case 'join':
+			words.shift();
+			var room = words.join(' ');
+
+			// Handle room changing/creating
+			this.changeRoom(room);
+			break;
+
+		case 'nick':
+			words.shift();
+			var name = words.join();
+
+			// Handle name-change attempts
+			this.socket.emit('nameAttempt', name);
+			break;
+
+		default:
+
+			// Return error message if command isn't recognized
+			message = 'Unrecognized command.';
+			break
+	}
+
+	return message;
+}
