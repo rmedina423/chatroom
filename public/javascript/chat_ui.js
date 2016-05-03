8edcb2cca35c7541106150cd4a94861eff9e789d
Ryan M. Medina <rmedina423@gmail.com>
2016-05-02 21:28:03 -0700

    initialize logic with `$(document).ready()`

--- public/javascript/chat_ui.js
+++ public/javascript/chat_ui.js
@@ -25,3 +25,65 @@ function processUserInput(chatApp, socket) {
 
 	$('#send-message').val();
 }
+
+var socket = io.connect();
+
+$(document).ready(function () {
+	var chatApp = new Chat(socket);
+
+	// Display results of a name-change attempt
+	socket.on('nameResult', function (result) {
+		var message;
+
+		if (result.success) {
+			message = 'You are now know as ' + result.name + '.';
+		} else {
+			message = result.message
+		}
+
+		$('#messages').append(divSystemContentElement(message));
+	});
+
+	// Display results of a room change
+	socket.on('joinResult', function (result) {
+		$('#room').text(result.room);
+		$('#messages').append(divSystemContentElement('Room changed.'));
+	});
+
+	// Display received messages
+	socket.on('message', function (message) {
+		var newElement = $('<div></div>').text(message.text);
+		$('#messages').append(newElement);
+	});
+
+	// Display list of rooms available
+	socket.on('rooms', function (rooms) {
+		$('#room-list').empty();
+
+		for (var room in rooms) {
+			room = room.substring(1, room.length);
+			if (room != '') {
+				$('#room-list').append(divEscapeContentElement(room));
+			}
+		}
+
+		// Allow click of a room name to change to that room
+		$('#room-list div').click(function() {
+			chatApp.processCommand('/join ' + $(this).text());
+			$('#send-message').focus();
+		})
+	});
+
+	// Request list of rooms available intermittently
+	setInterval(function () {
+		socket.emit('rooms');
+	}, 1000);
+
+	$('#send-message').focus();
+
+	// Allow submitting the form to send a chat message
+	$('#send-form').submit(function () {
+		processUserInput(chatApp, socket);
+		return false;
+	});
+});
